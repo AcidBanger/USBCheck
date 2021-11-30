233481e460830b1a4689711cf0c6d811e8e37798
Jeffrey van Houdt <jeffrey@123inkt.nl>
2021-11-30 12:48:39 +0100

    Add project files.

---
+++ USBCheck/Hoofdscherm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace USBCheck
+{
+    partial class Hoofdscherm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 19);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Hoofdscherm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 47);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Hoofdscherm";
+            this.Text = "Hoofdscherm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ USBCheck/Hoofdscherm.cs
@@ -0,0 +1,52 @@
+﻿//<div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+using System;
+using System.IO;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USBCheck
+{
+    public partial class Hoofdscherm : Form
+    {
+
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVNODES_CHANGED = 0X0007;
+
+        public Hoofdscherm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVNODES_CHANGED:
+                            listBox1.Items.Add("USB added/removed");
+                            ExampleAsync();
+                            break;
+                    }
+                    break;
+            }
+        }
+        public static async Task ExampleAsync()
+        {
+            using StreamWriter file = new("USB Check Log", append: true);
+            await file.WriteLineAsync("Device added/removed" + " " + Convert.ToString(WindowsIdentity.GetCurrent().Name) + " " + DateTime.Now.ToString());
+        }
+
+
+    }
+}
