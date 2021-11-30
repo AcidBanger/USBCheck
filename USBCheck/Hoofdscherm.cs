1a5efae08a0f92f7b4cb06b46ea3103c5539b5dd
Jeffrey van Houdt <jeffrey@123inkt.nl>
2021-11-30 14:05:21 +0100

    Added anoying sounds when disconnecting or connecting a USB device

--- USBCheck/Hoofdscherm.cs
+++ USBCheck/Hoofdscherm.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,6 +11,11 @@ namespace USBCheck
 {
     public partial class Hoofdscherm : Form
     {
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+
 
         private const int WM_DEVICECHANGE = 0x219;
         private const int DBT_DEVNODES_CHANGED = 0X0007;
@@ -43,10 +49,23 @@ namespace USBCheck
         }
         public static async Task ExampleAsync()
         {
-            using StreamWriter file = new("USB Check Log", append: true);
+            using StreamWriter file = new("USB_Check_Log.txt", append: true);
             await file.WriteLineAsync("Device added/removed" + " " + Convert.ToString(WindowsIdentity.GetCurrent().Name) + " " + DateTime.Now.ToString());
+
+            int n = 20;
+            for (int i = 1; i < n; i++)
+            {
+                SOUNDUP();
+                Console.Beep(800, 500);
+            }           
         }
 
-
+        public static int SOUNDUP()
+        {
+            int v = 500;
+            for (int i = 1; i < v; i++)
+                keybd_event((byte)Keys.VolumeUp, 0, 0, 0);
+            return 1;
+        }
     }
 }
